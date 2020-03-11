891f5fc3766e2c47ca3b3b3e4ed80e12f3fefbe3
Seikiko <31279305+seikiko613@users.noreply.github.com>
2020-03-11 22:34:57 +0800

    Update rwdphone.js

--- js/rwdphone.js
+++ js/rwdphone.js
@@ -7,8 +7,30 @@ function isMobile() {
 
 }
 
-if(isMobile()){/*....這裡就放入如果是行動裝置的話，要怎麼弄....*/
+if(isMobile()){
+    /*....這裡就放入如果是行動裝置的話，要怎麼弄....*/
 
 }else{
     /*....這裡就放入一般正常裝置的程式碼....*/
+}
+var rwd = window.matchMedia("(max-width: 815px)");
+rwd.addListener(resizeWidth);
+resizeWidth(rwd);
+
+function test1(){document.getElementById("tittle_link").style.display="none";
+document.getElementById("tittle_small").style.display="block";
+}
+function test2(){document.getElementById("tittle_link").style.display="block";
+document.getElementById("tittle_small").style.display="none";
+}
+
+function resizeWidth(pMatchMedia){
+  if (pMatchMedia.matches) {
+    //小於768時執行的js
+    test1();
+    
+  }else {
+    //大於768時執行的js
+    test2();
+  }
 }
