c0d8efa38299b8d6dfa62f827a95861ce0add6e4
Seikiko <31279305+seikiko613@users.noreply.github.com>
2020-03-11 22:34:55 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -4,21 +4,18 @@ window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
   if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
     document.getElementById("header").style.opacity = "0.7";
-      document.getElementsByClassName("content_sticker").style.display = "block";
+      
   } else {
     document.getElementById("header").style.opacity = "1";
-      document.getElementsByClassName("content_sticker").style.display = "none";
-  }
-}
-
-//滑鼠移到時觸發
-
-function StickerScroll() {
-  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-    document.getElementsByClassName("content_sticker").style.display = "block";
-  } else {
-    document.getElementsByClassName("content_sticker").style.display = "none";
       
   }
 }
 
+function test(){
+    var x = document.getElementById("tittle_link");
+    if(x.style.display==="none"){
+        x.style.display="block";
+    }else{
+        x.style.display="none";
+    }
+}
