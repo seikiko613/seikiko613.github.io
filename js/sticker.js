90aaf4997ac9eb84a49e85e60446db0b13ba4a33
Seikiko <31279305+seikiko613@users.noreply.github.com>
2020-03-14 20:53:13 +0800

    JQ調整

--- js/sticker.js
+++ js/sticker.js
@@ -1,9 +1,17 @@
+//載入後觸發
+$(document).ready(function(){
+    sticker();
+    getUrlParam();
+});
+
 
 // 給予貼圖購買連結並跑出貼圖
 var momo = '<p class="sticker_money">NT$30</p><a class="sticker_link" href="https://store.line.me/stickershop/product/1025308/zh-Hant?from=sticker"><div class="buy">購買</div></a><p class="sticker_p">讓默默兔的日常生活融入你的日常生活裡吧~</p>' ;
 var chicken = '<p class="sticker_money">NT$30</p><a class="sticker_link" href="https://store.line.me/stickershop/product/1120423/zh-Hant?from=sticker"><div class="buy">購買</div></a><p class="sticker_p">成功捕捉一隻野生雞哥！讓可愛的他參與你的日常貼圖吧 !</p>' ;
 var newyear = '<p class="sticker_money">NT$30</p><a class="sticker_link" href="https://store.line.me/stickershop/product/10227370/zh-Hant?from=sticker"><div class="buy">購買</div></a><p class="sticker_p">可愛的新年鼠陪伴“鼠”於你的新年</p>' ;
-window.onload = function sticker(){
+
+
+function sticker(){
     //默默兔和野生雞哥
     for(var i = 1; i <= 40 ; i++){
         if(i>=10){
@@ -29,6 +37,32 @@ window.onload = function sticker(){
     document.getElementById("sticker_newyear").innerHTML = newyear ;
 }
 
+
+// 判斷首頁點選的
+
+function getUrlParam(){
+    
+    var r = location.search;
+    if(r.indexOf('?')!= -1){//是否有資料傳遞
+        
+        var st = r.split('=');//st[0]='?sticker'
+        var sticker = "";
+        sticker = st[1];
+        
+        if (sticker ==  "momorabbit"){
+            show_momo();
+        }else
+        if (sticker == "wildchicken"){
+            show_chicken();
+        }else
+        if (sticker == "newyear"){
+            show_newyear();
+        }
+    }else{
+        show_momo();
+    }
+}
+
 //選擇顯示的貼圖
 //展示默默兔貼圖
 function show_momo(){
@@ -52,29 +86,7 @@ function show_newyear(){
     
 }
 
-// 判斷首頁點選的
 
 
-/*window.onload = function getUrlParam(){
-    
-    var r = location.search;
-    if(r.indexOf('?')!= -1){//是否有資料傳遞
-        
-        var st = r.split('=');//st[0]='?sticker'
-        var sticker = "";
-        sticker = st[1];
-        
-        if (sticker == "momorabbit"){
-            show_momo();
-        }
-        if (sticker == "wildchicken"){
-            show_chicken();
-        }
-        if (sticker == "newyear"){
-            show_newyear();
-        }
-    
-    }else{
-        show_momo();
-    }
-}*/
+
+
